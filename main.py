e7e7d146a0bea435338a3f52de618ad991380b2a
Long&Short <56889373+LongandShortio@users.noreply.github.com>
2020-04-09 15:22:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+os.chdir("") # Put your working directory
+from tgFills import *
+
+if __name__ == '__main__':
+    ws= websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
+    ws.run_forever()
